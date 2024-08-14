4f6a489a67053e627fce55a56878432aceeea892
iliya68rus <iliya97pro@yandex.ru>
2024-08-14 23:41:52 +0300

    hw-2

--- hw02-gradle/src/test/groovy/AtmTest.groovy
+++ hw02-gradle/src/test/groovy/AtmTest.groovy
@@ -1,18 +1,16 @@
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoSettings
 
 import static org.junit.jupiter.api.Assertions.assertEquals
 
+@MockitoSettings
 class AtmTest {
+    @InjectMocks
     private Atm atm
+    @Mock
     private Storage storage
-
-    @BeforeEach
-    void init() {
-        storage = Mockito.mock(Storage.class)
-        atm = new Atm(storage)
-    }
 
     @Test
     void acceptBanknotesSuccessfully() {
