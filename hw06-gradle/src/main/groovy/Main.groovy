a435800b086b5b090f0a77e2e3f0ff0785ea4adb
iliya68rus <iliya97pro@yandex.ru>
2024-09-12 15:19:06 +0300

    hw-6

--- hw06-gradle/src/main/groovy/Main.groovy
+++ hw06-gradle/src/main/groovy/Main.groovy
@@ -1,3 +1,5 @@
 static void main(String[] args) {
   println "Hello world!"
+  // https://github.com/Groovy-Developer/groovy-homeworks/blob/main/hw-5/test.json
+  // https://raw.githubusercontent.com/Groovy-Developer/groovy-homeworks/main/hw-5/test.json
 }
