c987060731dbfa2ab030277ba56ddc2843672ded
iliya68rus <iliya97pro@yandex.ru>
2024-11-13 13:31:39 +0300

    hw-11

--- hw11-gradle/src/main/groovy/QueryBuilder.groovy
+++ hw11-gradle/src/main/groovy/QueryBuilder.groovy
@@ -4,6 +4,7 @@ class QueryBuilder {
     def sql
     def entityClass
     def whereClause = []
+    def params = [:]
 
     QueryBuilder(Sql sql, Class entityClass) {
         this.sql = sql
@@ -33,13 +34,36 @@ class QueryBuilder {
         this
     }
 
+    def and(Closure closure) {
+        def andClause = []
+        closure.delegate = this
+        closure.resolveStrategy = Closure.DELEGATE_FIRST
+        closure()
+        if (andClause.size() > 0) {
+            whereClause.add('(' + andClause.join(' AND ') + ')')
+        }
+        this
+    }
+
     def eq(String column, Object value) {
-        whereClause.add("${column} = ${value}")
+        def paramName = "param_${params.size()}"
+        if (value == null) {
+            whereClause.add("${column} IS :${paramName}")
+        } else {
+            whereClause.add("${column} = :${paramName}")
+        }
+        params[paramName] = value
         this
     }
 
     def nonEq(String column, Object value) {
-        whereClause.add("${column} IS NOT ${value}")
+        def paramName = "param_${params.size()}"
+        if (value == null) {
+            whereClause.add("${column} IS NOT :${paramName}")
+        } else {
+            whereClause.add("${column} != :${paramName}")
+        }
+        params[paramName] = value
         this
     }
 
@@ -55,9 +79,8 @@ class QueryBuilder {
     def execute() {
         def query = buildQuery()
         println "Выполненый запрос: ${query}"
-        sql.eachRow(query.toString()) { row ->
+        sql.rows(query, params) { row ->
             println row
         }
     }
 }
-
